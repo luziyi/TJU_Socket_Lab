a844b9c13a97cea350b5afc1f929bed1d5265e88
icey-cola <574583469@qq.com>
2024-05-22 02:43:05 +0000

    head已解决 get未解决

--- project/include/response.h
+++ project/include/response.h
@@ -4,13 +4,22 @@
 #include "parse.h"
 
 #define MAX_MESSAGE_LENGTH 4096
-
+#define GET_SUCCESS 1
+#define GET_FAILURE 0
+#define URL_MAX_SIZE 256
+#define O_RDONLY 00
+#define S_ISREG 0100000
+#define S_IRUSR 00400
+#define BUF_SIZE 4096
 char RESPONSE_400[4096] = "HTTP/1.1 400 Bad request\r\n\r\n";
 char RESPONSE_404[4096] = "HTTP/1.1 404 Not Found\r\n\r\n";
 char RESPONSE_501[4096] = "HTTP/1.1 501 Not Implemented\r\n\r\n";
 char RESPONSE_505[4096] = "HTTP/1.1 505 HTTP Version not supported\r\n\r\n";
 char RESPONSE_200[4096] = "HTTP/1.1 200 OK\r\n\r\n";
-
+char http_version_now[50] = "HTTP/1.1";
+char root_path[50] = "./static_site";
+char file_path[50] = "/index.html";
+ 
 char *Response(char *message_buffer, int complete_message_length, int client_sock)
 {
     char *response_message;
@@ -27,8 +36,68 @@ char *Response(char *message_buffer, int complete_message_length, int client_soc
     }
     else if (strcmp(request->http_method, "GET") == 0)
     {
+        char buf[BUF_SIZE];
         // Handle GET request
-        FILE *file = fopen("/webServerStartCodes-new/static_site/index.html", "r");
+            char head_URL[BUF_SIZE];
+            memset(head_URL, 0, sizeof(head_URL));
+            strcat(head_URL, root_path);
+            int head_flag = GET_SUCCESS;
+            if (strcmp(request->http_uri, "/") == 0)
+            {
+                strcat(head_URL, file_path);
+            }
+            else if (sizeof(request->http_uri) + sizeof(root_path) < URL_MAX_SIZE)
+            {
+                strcat(head_URL, request->http_uri);
+            }
+            else
+            {
+                head_flag = GET_FAILURE;
+                memset(buf, 0, sizeof(buf));
+                memcpy(buf, "HTTP/1.1 404 Not Found\r\n\r\n", sizeof("HTTP/1.1 404 Not Found\r\n\r\n"));
+                send(client_sock, buf, readret, 0);
+            }
+            
+            struct stat *file_state = (struct stat *)malloc(sizeof(struct stat));
+            if (stat(head_URL, file_state) == -1)
+            {
+                return GET_FAILURE;
+            }
+            
+            int fd_in = open(head_URL, O_RDONLY);
+            if (!(S_ISREG & file_state->st_mode) || !(S_IRUSR & file_state->st_mode))
+            {
+                return GET_FAILURE;
+            }
+            
+            if (fd_in < 0)
+            {
+                printf("Failed to open the file\n");
+                return GET_FAILURE;
+            }
+
+            char response1[BUF_SIZE];
+            char response2[BUF_SIZE];
+            ssize_t bytesRead = read(fd_in, response2, BUF_SIZE);
+            if (bytesRead < 0)
+            {
+                printf("Failed to read the file\n");
+                close(fd_in);
+                free(file_state);
+                return GET_FAILURE;
+            }
+            
+            memcpy(response1, "HTTP/1.1 200 OK\r\n\r\n", sizeof(response1));
+            strncat(response1, response2, bytesRead);
+            send(client_sock, response1, bytesRead + strlen("HTTP/1.1 200 OK\r\n\r\n"), 0);
+
+            close(fd_in);
+            free(file_state);
+               
+            free(request->headers);
+            free(request);
+
+       /* FILE *file = fopen("/webServerStartCodes-new/static_site/index.html", "r");
         if (file == NULL) {
             response_message = RESPONSE_404;
         } else {
@@ -40,18 +109,11 @@ char *Response(char *message_buffer, int complete_message_length, int client_soc
             strcpy(response_message, RESPONSE_200);
             fread(response_message + sizeof(RESPONSE_200) - 1, file_size, 1, file);
             fclose(file);
-        }   
+        }   */
     }
     else if (strcmp(request->http_method, "HEAD") == 0)
     {
-        // Handle HEAD request
-        FILE *file = fopen("/webServerStartCodes-new/static_site/index.html", "r");
-        if (file == NULL) {
-            response_message = RESPONSE_404;
-        } else {
-            fclose(file);
-            response_message = RESPONSE_200;
-        }
+        response_message = "HTTP/1.1 200 OK\r\n\r\n";;
     }
     else if (strcmp(request->http_method, "POST") == 0)
     {
